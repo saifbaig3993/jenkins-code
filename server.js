c59f7f20b37028cf549b3e2534039331b2ebf64c
Saif Biag <saif.baig@khazanapk.com>
2025-08-26 16:54:12 +0500

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('My First Containerized Node second app');
+  res.send('My First Containerized Node second app new');
 });
 
 app.listen(PORT, HOST, () => {
